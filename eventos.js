862122eba1542210b46b9b29a45f93e7e45fa7e9
irlisson-23ferreira <irlissondias@gmail.com>
2017-04-12 18:15:49 +0000

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function eventoentrar(){
+   alert("seja bem vindo!");
+ }
+ function eventosair(){
+  alert("até a próxima visita");
+ }
+   function eventoClique(){
+  window.document.getElementById('id2').style.color='red';
+ }
